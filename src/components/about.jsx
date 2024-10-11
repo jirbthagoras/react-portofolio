d8f39e8a8f3e75e64764d67a33c2fb94327ebb8a
jirbthagoras <hansjabriel@gmail.com>
2024-10-11 21:19:52 +0700

    [+] Adding About Page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
 import siRajaIblis from "../assets/WhatsApp Image 2024-10-10 at 2.18.48 PM.jpeg"
+import bg from "../assets/Download Red grunge texture banner for free.jpeg"
 
 const About = () => {
     return (
-        <section id="about" className="py-20 bg-gray-100">
-            <div className="container mx-auto text-center px-6">
-                <h2 className="text-4xl font-bold mb-8 text-gray-800">
-                    Ini Gua
-                </h2>
+        <section id="about" className="py-20 bg-cover h-auto bg-red-800">
 
-                <div className="flex flex-col items-center mb-12">
-                    <img src={`${siRajaIblis}`} alt="RajaIblis" className={"w-36 h-36 rounded-full object-cover mb-4 shadow-lg"}/>
-                    <p className={"text-lg text-gray-600 mb-2"}>Software Engineer</p>
-                    <p className={"text-lg text-gray-600 mb-2"}>Fulltime Schizophrenic Guy</p>
+            <div className="container mx-auto text-center px-6">
+
+                <h2 className="text-4xl font-bold text-white font-mono">Tentang saya:</h2>
+
+                <div className="flex-col flex items-center mb-12">
+                    <img src={`${siRajaIblis}`} alt="RajaIblis"
+                         className={"mt-2 w-60 h-60 rounded-full object-cover mb-4 shadow-lg"}/>
+                    <p className={"text-2xl font-bold text-white font-mono mb-2"}>Software Engineer</p>
+                    <p className={"text-2xl text-white font-semibold"}>Fulltime Schizophrenic Guy</p>
                 </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    <div
+                        className="bg-white p-6 rounded-lg shadow-lg hover:border-gray-50 transition-colors duration-300">
+                        <h3 className="text-2xl font-bold mb-4 text-rose-700">Backend Development</h3>
+                        <p className="text-gray-700 mb-4">
+                            I Develop A Server Side Application That Works Well.
+                        </p>
+                    </div>
+
+                    <div
+                        className="bg-white p-6 rounded-lg shadow-lg hover:border-gray-50 transition-colors duration-300">
+                        <h3 className="text-2xl font-bold mb-4 text-rose-700">Web Development</h3>
+                        <p className="text-gray-700 mb-4">
+                            Even Though I Can't Do The Client-Side Well, I Still Can Make A MVP WebApp
+                        </p>
+                    </div>
+                </div>
+                <p className="text-4xl mt-8 text-white font-bold">
+                    I Do Also Mentoring Backend Class At SnapanDev Community
+                </p>
             </div>
         </section>
     )
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -9,7 +9,7 @@ const Hero = () =>
 
             <div className="bg-black bg-opacity-60 h-full flex flex-col justify-center items-center text-center p-6">
                 <h1 className="text-6xl font-bold mb-4">Yo, I'm Jabriel</h1>
-                <p className="text-2xl mb-8">Aku suka <strong className="text-rose-700 bg-white rounded">Laravel</strong> tapi sayangnya ini dibuat make <strong className="text-teal-500 bg-white rounded">React</strong>
+                <p className="text-2xl mb-8">Aku sukanya <strong className="text-rose-700 bg-white rounded">Laravel</strong> tapi sayangnya ini dibuat make <strong className="text-teal-500 bg-white rounded">React</strong>
                 </p>
                 <a href="#projects" className="bg-rose-700 hover:bg-rose-500 text-white font-bold py-2 px-6 rounded-lg shadow-lgm transition duration-300">Lihat Projekku!</a>
             </div>
