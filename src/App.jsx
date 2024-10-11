655e6427b356b97b40a83912179d970fa82153d8
jirbthagoras <hansjabriel@gmail.com>
2024-10-11 20:08:26 +0700

    [+] First Init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from "./components/navbar.jsx";
+import Hero from "./components/hero.jsx";
+
+export default function App() {
+    return (
+        <h1>
+            <Navbar/>
+            <Hero/>
+        </h1>
+    )
+}
---
+++ src/components/navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import mbakAsai from '../assets/asai.jpeg'
+
+const Navbar = () =>
+{
+    return (
+        <nav className="bg-red-800 p-4">
+            <div className="container mx-auto flex items-center justify-between">
+                <div>
+                    <img src={`${mbakAsai}`} alt="Ini Asai"
+                         className="object-scale-down h-20 max-w-full drop-shadow-md rounded-md"/>
+
+                </div>
+                <div>
+                    <h1 className="text-5xl font-bold font-mono text-white">jirb.php</h1>
+                </div>
+                <div>
+                    <a href="#about" className="font-mono text-2xl font-bold text-gray-300 hover:text-white mx-4 transition duration-300">about</a>
+                    <a href="#projects" className="font-mono text-2xl font-bold text-gray-300 hover:text-white mx-4 transition duration-300">project</a>
+                    <a href="#contact" className="font-mono text-2xl font-bold text-gray-300 hover:text-white mx-4 transition duration-300">contact</a>
+                </div>
+
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar;
